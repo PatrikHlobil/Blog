684bcf4dcd861ec3159315bc8346cdfb85d11889
Patrik.Hlobil <patrik.hlobil@dm.de>
2023-03-11 12:19:41 +0100

    fix blog generation

--- blog_generator/generator/blogs.py
+++ blog_generator/generator/blogs.py
@@ -18,21 +18,28 @@ class BlogData:
 
 
 def render_blogs(blogs_directory: Path, target_dir: Path):
+    _prepare_target_directory(target_dir)
+    for blog_data in _get_blog_data(blogs_directory):
+        if blog_data.draft is False:
+            _render_and_export_blog(blog_data, target_dir)
+
+
+def _render_and_export_blog(blog_data: BlogData, target_dir: Path):
+    blog_template = _get_blog_template()
+    rendered_blog = blog_template.render(blog_data=blog_data)
+    (target_dir / blog_data.file_name).write_text(rendered_blog)
+
+
+def _prepare_target_directory(target_dir: Path):
+    target_dir.mkdir(exist_ok=True)
     for path in target_dir.glob("*.md"):
         path.unlink()
-    blog_template = _get_blog_template()
-    for blog_data in _get_blog_data(blogs_directory):
-        if blog_data.draft is True:
-            continue
-        rendered_blog = blog_template.render(blog_data=blog_data)
-        (target_dir / blog_data.file_name).write_text(rendered_blog)
 
 
 def _get_blog_data(blogs_directory: Path) -> Iterator[BlogData]:
     markdown_processor = markdown.Markdown(extensions=["meta"])
     for blog_path in blogs_directory.glob("*.md"):
         markdown_processor.convert(blog_path.read_text())
-
         yield BlogData(
             file_name=blog_path.name,
             content="\n".join(markdown_processor.lines),
